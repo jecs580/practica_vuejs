8c4da41af8ee9f332bcd5f38a42887c980983daf
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-24 20:05:45 -0400

    Manejo de clases de CSS desde vuejs

--- main.js
+++ main.js
@@ -1,43 +1,9 @@
 var app = new Vue({
     el:'#app',
     data:{
-        saludo:'Hello world from Vuejs',
-        fruits:[
-            {
-            'name':'apple',
-            'cantidad':10
-            },
-            {
-            'name':'banana',
-            'cantidad':3
-            },
-            {
-            'name':'orange',
-            'cantidad':0
-            }
-        ],
-        newFruit:'',
-        total:0
+       fondo:'bg-warning',
+       color:true
     },
     methods:{
-        addFruit(){
-            if(this.newFruit!=''){
-                this.fruits.push({
-                    name:this.newFruit,
-                    cantidad:0
-                })
-                this.newFruit='';
-            }
-        }
     },
-    computed:{
-        // Ejecuta una funcion cada vez q cambien los datos que esta dentro de su funcion
-        sumarFruit(){
-            this.total=0;
-            for (const fruit of this.fruits) {
-                this.total+=fruit.cantidad;
-            }
-            return this.total;
-        }
-    }
 })
