e7e21bb8aca432559eedd5c60337d886fe47d340
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-25 20:35:37 -0400

    Creacion de aplicacion de tareas

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 var app = new Vue({
     el:'#app',
     data:{
-       fondo:'bg-warning',
-       color:true
+       title:'GYM con Vue',
+       tasks:[
+
+       ],
+       newTask:''
     },
     methods:{
+        addTask(){
+            this.tasks.push({
+                name:this.newTask,
+                state:false
+            });
+            this.newTask=''
+            localStorage.setItem('gym-local',JSON.stringify(this.tasks))
+        },
+        doneTask(i){
+            this.tasks[i].state=true;
+            localStorage.setItem('gym-local',JSON.stringify(this.tasks))
+        },
+        deleteTask(i){
+            this.tasks.splice(i,1);          
+            localStorage.setItem('gym-local',JSON.stringify(this.tasks))
+        }
     },
+    created(){
+        let datosLS=JSON.parse(localStorage.getItem('gym-local'));
+        if(datosLS=== null){
+            this.tasks=[];
+        }else{
+            this.tasks=datosLS;
+        }
+    }
 })
