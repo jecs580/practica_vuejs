a07907acd9eedf3ed2bffbd4c638915898274fbc
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-26 23:21:27 -0400

    Ejemplos con propiedades computadas

--- main.js
+++ main.js
@@ -1,36 +1,22 @@
 var app = new Vue({
     el:'#app',
     data:{
-       title:'GYM con Vue',
-       tasks:[
-
-       ],
-       newTask:''
+        message:"Hi, I'm Jorge",
+        contador:0,
     },
     methods:{
-        addTask(){
-            this.tasks.push({
-                name:this.newTask,
-                state:false
-            });
-            this.newTask=''
-            localStorage.setItem('gym-local',JSON.stringify(this.tasks))
-        },
-        doneTask(i){
-            this.tasks[i].state=true;
-            localStorage.setItem('gym-local',JSON.stringify(this.tasks))
-        },
-        deleteTask(i){
-            this.tasks.splice(i,1);          
-            localStorage.setItem('gym-local',JSON.stringify(this.tasks))
-        }
+        
     },
-    created(){
-        let datosLS=JSON.parse(localStorage.getItem('gym-local'));
-        if(datosLS=== null){
-            this.tasks=[];
-        }else{
-            this.tasks=datosLS;
+    computed:{
+        invertido(){
+            return this.message.split('').reverse().join('');
+        },
+        color(){
+            return{
+                'bg-danger':this.contador<=10,
+                'bg-warning':this.contador>10 && this.contador<=20,
+                'bg-success':this.contador>20
+            }
         }
     }
 })
