5ed851fbc588053d6628056cc1fd124583babc04
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-27 23:46:55 -0400

    Ciclo de vida de Vuejs

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 var app = new Vue({
     el:'#app',
     data:{
-        message:"Hi, I'm Jorge",
-        contador:0,
+        saludo:'Ciclo de vida de Vuejs'
+    },
+    beforeCreate(){
+        console.log('BeforeCreate');
+    },
+    created(){
+        // Se ejecuta cuando ya estan creados los metodos, eventos, pero aun no se puede acceder al DOM
+        console.log('Created');
+    },
+    beforeMount(){
+        // Se ejecuta antes de insertar al DOM (Antes que se use Vuejs en el html)
+        console.log('BeforeMount');
+    },
+    mounted(){
+    //    Se ejecuta al insertar el DOM 
+        console.log('Mounted');
+    },
+    beforeUpdate(){
+        // Al detectar un cambio en el Html o en la instancia de Vuejs
+        console.log('BeforeUpdate');
+    },
+    updated(){
+        // Al realizar un cambio en el Html o en la instancia de Vuejs
+        console.log('Updated');
+    },
+    beforeDestroy(){
+        // Antes de destruir la instacion
+        console.log('BeforeDestroy');
+    },
+    destroyed(){
+        // Se destruye toda la instancia
+        console.log('destroyed');
     },
     methods:{
-        
+        destruir(){
+            this.$destroy();
+        }
     },
     computed:{
-        invertido(){
-            return this.message.split('').reverse().join('');
-        },
-        color(){
-            return{
-                'bg-danger':this.contador<=10,
-                'bg-warning':this.contador>10 && this.contador<=20,
-                'bg-success':this.contador>20
-            }
-        }
+        
     }
 })
