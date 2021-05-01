df3afab63b48a565875fd84aefadc0b748ff58f2
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-30 23:55:41 -0400

    Comunicacion entre componentes hijo-padre

--- components/hijo.js
+++ components/hijo.js
@@ -2,7 +2,16 @@ Vue.component('hijo',{
     template:`
         <div class="py-5 bg-success">
             <h4>Componente hijo {{numero}}</h4>
+            <h4>Nombre: {{name}}</h4>
         </div>
     `,
-    props:['numero']
+    props:['numero'],
+    data(){
+        return{ 
+            name:'Jorge Callisaya',
+        }
+    },
+    mounted(){
+        this.$emit('nameChild',this.name);
+    }
 })
--- components/padre.js
+++ components/padre.js
@@ -3,12 +3,14 @@ Vue.component('padre',{
     <div class="p-5 bg-dark text-white">
     <h2>Componente Padre {{numeroPadre}}</h2>
     <button class="btn btn-danger" @click="numeroPadre++">+</button>
-    <hijo :numero="numeroPadre"></hijo>
+    <hijo :numero="numeroPadre" @nameChild="nameFromChild=$event"></hijo>
+    {{nameFromChild}}
     </div>
     `,
     data(){
         return {
-            numeroPadre:0
+            numeroPadre:0,
+            nameFromChild: '',
         };
     }
 })
