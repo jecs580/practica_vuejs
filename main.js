3a9268ee83d99c2b9cbe7cea7605f81d5dd93f8b
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-01 21:59:43 -0400

    Uso de Vuex para comunicacion entre componentes

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-
+Vue.component('titulo',{
+    template:`
+      <div>
+        <h1>numero {{$store.state.numero}}</h1>
+        <hijo></hijo>
+      </div>
+    `
+});
+Vue.component('hijo',{
+    template:`
+    <div>
+        <h1>numero {{$store.state.numero}}</h1>
+        <button @click="$store.commit('add')">+</button>
+  </div>
+    `
+});
+const store = new Vuex.Store({
+    state:{
+        numero:10
+    },
+    mutations:{
+        add(state){
+            state.numero++;
+        }
+    }
+});
 var app = new Vue({
     el:'#app',
+    store,
     data:{
         saludo:'Vuejs'
     },
